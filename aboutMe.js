b545200d5bf4594dc0d48b36edbf80235fac83ff
Raymond Chen <149682536+rayc6564@users.noreply.github.com>
2024-07-16 15:29:03 -0400

    Add files via upload

--- aboutMe.js
+++ aboutMe.js
@@ -24,9 +24,9 @@ educationBtn.addEventListener("click", () =>{
 
 document.getElementById('menu-btn').addEventListener('click', function() {
     const dropdown = document.querySelector('.dropdown-btns');
-    if (dropdown.style.display === 'flex') {
+    if (dropdown.style.display === 'block') {
         dropdown.style.display = 'none';
     } else {
-        dropdown.style.display = 'flex';
+        dropdown.style.display = 'block';
     }
 });
--- resume.js
+++ resume.js
@@ -1,8 +1,8 @@
 document.getElementById('menu-btn').addEventListener('click', function() {
     const dropdown = document.querySelector('.dropdown-btns');
-    if (dropdown.style.display === 'flex') {
+    if (dropdown.style.display === 'block') {
         dropdown.style.display = 'none';
     } else {
-        dropdown.style.display = 'flex';
+        dropdown.style.display = 'block';
     }
 });
