90751916f5a40dfb7e4c825e0a5b0f373291e3ed
Raymond Chen <149682536+rayc6564@users.noreply.github.com>
2024-11-15 12:36:48 -0500

    Delete resume.js

--- resume.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('menu-btn').addEventListener('click', function() {
-    const dropdown = document.querySelector('.dropdown-btns');
-    if (dropdown.style.display === 'block') {
-        dropdown.style.display = 'none';
-    } else {
-        dropdown.style.display = 'block';
-    }
-});
